c0ce4a76679fca309afc97ecb0c0dc7ad4b08ebc
zeze <thwjd2717@gmail.com>
2023-04-23 13:58:49 +0900

    add: required_login 기능이 필요한 api 사용시 회원정보 가져옴

--- api.py
+++ api.py
@@ -1,6 +1,7 @@
 from flask import current_app
 from sqlalchemy import text
 
+
 def get_user(user_id):
     user = current_app.database.execute(text("""
         SELECT
@@ -17,6 +18,7 @@ def get_user(user_id):
         'profile' : user['profile']
     } if user else None
 
+
 def insert_user(user):
     return current_app.database.execute(text("""
         INSERT INTO users (
@@ -32,6 +34,7 @@ def insert_user(user):
         )
     """), user).lastrowid
 
+
 def insert_tweet(user_tweet):
     return current_app.database.execute(text("""
         INSERT INTO tweets (
@@ -42,6 +45,7 @@ def insert_tweet(user_tweet):
             :tweet
         )
     """), user_tweet).rowcount
+
 
 def insert_follow(user_follow):
     return current_app.database.execute(text("""
@@ -54,12 +58,14 @@ def insert_follow(user_follow):
         )
     """), user_follow).rowcount
 
+
 def insert_unfollow(user_unfollow):
     return current_app.database.execute(text("""
         DELETE FROM user_follow_list
         WHERE user_id = :id
         AND follow_user_id = :unfollow
     """), user_unfollow).rowcount
+
 
 def get_timeline(user_id):
     timeline = current_app.database.execute(text("""
@@ -77,3 +83,17 @@ def get_timeline(user_id):
         'user_id': tweet['user_id'],
         'tweet': tweet['tweet']
     } for tweet in timeline]
+
+def get_user_id_and_password(email):
+    row = current_app.database.execute(text("""    
+        SELECT
+            id,
+            hashed_password
+        FROM users
+        WHERE email = :email
+    """), {'email' : email}).fetchone()
+
+    return {
+        'id'              : row['id'],
+        'hashed_password' : row['hashed_password']
+    } if row else None
